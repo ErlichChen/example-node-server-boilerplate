7b4c88bf2fa3f98125e40e49f2a714856036f0f2
erlichchen <chenxuewei.dev@gmail.com>
2019-04-23 09:00:15 +0800

    gulp babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
-const { series } = require('gulp');
+const gulp = require('gulp');
+const { src, dest, series } = require('gulp');
+const babel = require('gulp-babel');
+const del = require('del');
 
-function clean(cb) {
-  cb();
-  console.log('clean');
+const paths = {
+  src: ['src/**/*.js'],
+  test: ['src/**/test*.js'],
+  build: 'build'
+}
+
+function clean() {
+  return del(['build'])
 }
 
 function build(cb) {
-  cb();
-  console.log('build');
+  return src(paths.src)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.build));
 }
 
 function watch(cb) {
